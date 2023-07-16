5e21e16af07361bcc98f9e0f616283bc9d216a88
Danujaya  Liyanage <2021is054@stu.ucsc.cmb.ac.lk>
2023-07-16 15:04:49 +0530

    add styles and index page

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <title>rps</title>
+   <title>Rock Paper Scissors</title>
 </head>
 <body>
-   
+   <div>
+        <h1>Welcome to Rock Paper Scissors</h1>
+        <p>
+             <a href="login.php">Please Log In</a>
+        </p>
+   </div>
 </body>
 </html>
